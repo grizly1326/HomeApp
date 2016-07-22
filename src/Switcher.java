853d2224afde581946508fe809dcacf51eb35617
grizly1326 <daniel.maly@hotmail.com>
2016-07-22 18:42:37 +0200

    22/7/2016 commit.

--- src/Switcher.java
+++ src/Switcher.java
@@ -9,15 +9,17 @@ import java.util.ArrayList;
 public class Switcher {
 	public static void mainSwitch(DatagramPacket packet){
         String a=new String(packet.getData()).substring(0, packet.getLength());
-        System.out.println(a);
-        switch(a){
+        String[] sub=a.split("=");
+        switch(sub[0]){
             case "ping":System.out.println("ping from here.");Send.sendMessage(packet.getAddress(),"ping.back"); break;
             case "ping.getContent":Send.sendMessage(packet.getAddress(),"ping.getContent=Video player");break;
-            case "getList":System.out.println(a);send_list(packet.getAddress(),"list.txt");break;
-            default:System.out.println("Cannot find this command:    "+a);break;
+            case "getList":send_file(packet.getAddress(),"list.txt");break;
+            case "getMedia":send_file(packet.getAddress(),sub[1]);break;
+            default:System.out.println("Cannot find this command:    "+sub[0]);break;
         }
     }
-	private static void send_list(InetAddress address,String FileName){
+	private static void send_file(InetAddress address,String FileName){
+		System.out.println(FileName);
 		SendFile a = null;
 		try {
 			a = new SendFile(new Socket(address,1325));
